14f80f76be02eb139a4ec9a1f4b0cb644facb256
Obando96 <joseobando30@gmail.com>
2023-04-26 00:41:30 -0500

    Primer commit

---
+++ script.js
@@ -0,0 +1,58 @@
+function seleccionar(link) {
+  var opciones = document.querySelectorAll("#links  a");
+  opciones[0].className = "";
+  opciones[1].className = "";
+  opciones[2].className = "";
+  opciones[3].className = "";
+  link.className = "seleccionado";
+
+  var x = document.getElementById("nav");
+  x.className = "";
+}
+
+function responsiveMenu() {
+  var x = document.getElementById("nav");
+  if (x.className === "") {
+    x.className = "responsive";
+  } else {
+    x.className = "";
+  }
+}
+
+window.onscroll = function () {
+  efectoHabilidades();
+};
+
+function efectoHabilidades() {
+  var skills = document.getElementById("skills");
+  var distancia_skills =
+    window.innerHeight - skills.getBoundingClientRect().top;
+  if (distancia_skills >= 300) {
+    document.getElementById("html").classList.add("barra-progreso1");
+    document.getElementById("js").classList.add("barra-progreso2");
+    document.getElementById("react").classList.add("barra-progreso3");
+    document.getElementById("node").classList.add("barra-progreso4");
+  }
+}
+
+const btn = document.getElementById("buttonEnviar");
+
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  btn.value = "Enviando...";
+
+  const serviceID = "default_service";
+  const templateID = "template_bokwylh";
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Enviar Mensaje";
+      alert("Mensaje enviado correctamente!");
+    },
+    (err) => {
+      btn.value = "Enviar Mensaje";
+      alert(JSON.stringify(err));
+    }
+  );
+});
